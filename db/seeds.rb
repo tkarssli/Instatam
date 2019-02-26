91e2ffa43eae81bb7d67999a1c196abccf3bf6f7
Tam Kar <tkarssli@gmail.com>
2019-02-26 13:28:05 -0800

    Initialize seeds file with transacction

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ActiveRecord::Base.transaction do
+    
+end
