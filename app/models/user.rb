54251cdb5347f4de59135aacb3c5d687b3acc2a4
Tam Kar <tkarssli@gmail.com>
2019-02-26 11:45:31 -0800

    Add User Auth to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,48 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
+    attr_reader :password
+    validates :username, :email, :session_token, uniqueness: true, presence: true 
+    validates :password_digest, presence: true
+    validates :password, length: {minimum: 8, allow_nil: true}
+
+    def self.find_by_credentials(username, password)
+        @user = User.find_by(username: username)
+        if @user && @user.is_password?(password)
+            return @user
+        else
+            nil
+        end
+    end
+    
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+    
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+    
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64()
+    end
+    
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64()
+        self.save!
+        return self.session_token
+    end
+    
 end
