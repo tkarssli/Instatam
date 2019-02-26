a1060a655a6ff9599bbba5657fe6392f46f03c59
Tam Kar <tkarssli@gmail.com>
2019-02-26 14:50:52 -0800

    Setup front end

--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! user, :id, :username, :email, :session_token
+# json.extract! user, :id, :username, :email, :session_token
---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import entitiesReducer from './entities_reducer';
+import errorsReducer from './errors_reducer';
+
+export default combineReducers({
+})
