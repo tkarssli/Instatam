cee5acc67508cb66e03d5b93a19786fb450d7065
Tam Kar <tkarssli@gmail.com>
2019-02-28 17:32:25 -0800

    make corrections for Daddy Ronil

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -25,68 +25,70 @@ class SessionForm extends React.Component {
         e.preventDefault();
         const user = Object.assign({}, this.state)
         this.props.processForm(user)
-        if(!this.props.errors){
+        if (!this.props.errors) {
             this.props.history.push('/')
         }
     }
 
     handleDemoLogin(e) {
-        const demo_user = {username:"Demo User", password: "password"}
+        const demo_user = { username: "Demo User", password: "password" }
         this.props.demoLogin(demo_user)
         this.props.history.push('/')
     }
 
     update(field) {
         return (e) => {
-            this.setState({[field]: e.target.value})
+            this.setState({ [field]: e.target.value })
         }
     }
 
 
 
     render() {
-        const { errors, formType} = this.props
+        const { errors, formType } = this.props
         return (
-             <>
-                            
-                {(formType === 'signup' )? (<p>Sign up to see photos and videos from your friends</p>): ""}
+            <>
+
+                {(formType === 'signup') ? (<p>Sign up to see photos and videos from your friends</p>) : ""}
                 <form className="userauth" onSubmit={this.handleSubmit}>
-                <ul className="errors">
-                    {errors.map(error => <li>{error}</li>)}
-                </ul>
 
-                <input 
-                    type="text" 
-                    onChange={this.update('username')} 
-                    value={this.state.username}
-                    placeholder="Username"
 
-                />
-                {(formType === 'signup') ? (
-                    <input 
-                        type="text" 
-                        onChange={this.update('email')} 
-                        value={this.state.email}
-                        placeholder="Email"
+                    <input
+                        type="text"
+                        onChange={this.update('username')}
+                        value={this.state.username}
+                        placeholder="Username"
 
                     />
-                ) : ""}
-                <input 
-                    type="password" 
-                    onChange={this.update('password')} 
-                    value={this.state.password}
-                    placeholder="Password"
-                />
+                    {(formType === 'signup') ? (
+                        <input
+                            type="text"
+                            onChange={this.update('email')}
+                            value={this.state.email}
+                            placeholder="Email"
 
-                <input className="btn" type="submit" value={formType.charAt(0).toUpperCase() + formType.slice(1)}/>
-                <button className="btn" onClick={this.handleDemoLogin}>Demo User</button>
-                    
+                        />
+                    ) : ""}
+                    <input
+                        type="password"
+                        onChange={this.update('password')}
+                        value={this.state.password}
+                        placeholder="Password"
+                    />
+
+                    <input className="btn" type="submit" value={formType.charAt(0).toUpperCase() + formType.slice(1)} />
+                    <button className="btn" onClick={this.handleDemoLogin}>Demo User</button>
+                    <ul className="errors">
+                        {errors.map(error => <li>{error}</li>)}
+                    </ul>
+                    {(formType === 'signup') ? (<p>By signing up, you agree to our Terms , Data Policy and Cookies Policy .</p>) : ""}
+
                 </form>
-             </>
+            </>
         );
     }
 
-   
+
 }
 
 export default withRouter(SessionForm);
