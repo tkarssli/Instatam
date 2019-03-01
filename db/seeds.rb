8916ed1f54e14717b530363071334dc3f9050881
Tam Kar <tkarssli@gmail.com>
2019-03-01 11:25:45 -0800

    Create seeds for User and Post objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,32 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
+
 ActiveRecord::Base.transaction do
 
+    User.destroy_all
+
+    # DON'T DELETE #######################################################################
     demo = User.create(username: "Demo User", email: "Demo@User.com", password: "password")
-    
+    ######################################################################################
+
+    u1 = User.create(username: "Tamir", email: "Tamir@Karssli.com", password: "password")
+    u2 = User.create(username: "Dorotea", email: "Dorotea@Italy.com", password: "password")
+    u3 = User.create(username: "Zane", email: "Zane@Music.com", password: "password")
+
+    users = []
+    (0..20).each do 
+        users.push(User.create(username: Faker::Name.unique.name, email: Faker::Internet.unique.email, password: "password"))
+    end
+
+    Post.destroy_all
+
+    (0...23).each do |n|
+        filename = "seed#{n}"
+        file = File.open("seed/#{filename}.jpg")
+        user = users.sample
+        post = Post.create(caption: Faker::Hipster.unique.sentence, user_id: user.id)
+        post.photo.attach(io: file, filename: filename)
+    end
 end
