0aa714f20d13526e447e232b20e18fab13f21d19
Tam Kar <tkarssli@gmail.com>
2019-03-01 17:07:43 -0800

    Add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   context: __dirname,
@@ -8,22 +9,27 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          query: {
-            presets: ['@babel/env', '@babel/react']
-          }
-        },
-      }
-    ]
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/env', '@babel/react']
+        }
+      },
+    }]
   },
   devtool: 'inline-source-map',
   resolve: {
     extensions: [".js", ".jsx", "*"]
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      // relative path is from src
+      {
+        from: './app/assets/images/favicon.ico'
+      }, // <- your path to favicon
+    ])
+  ]
 };
-
