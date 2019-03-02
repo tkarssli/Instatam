9d82d2ad01d803cdabf2c7ec213f3e573c248b1c
Tam Kar <tkarssli@gmail.com>
2019-03-01 18:37:54 -0800

    Create util methods for posts

---
+++ frontend/util/post_api_util.js
@@ -0,0 +1,39 @@
+export const fetchPosts = () => (
+    $.ajax({
+        method: "GET",
+        url: "api/posts",
+    })
+)
+
+export const fetchPost = id => (
+    $.ajax({
+        method: "GET",
+        url: `/api/posts/${id}`,
+    })
+)
+
+export const createPost = post => (
+    $.ajax({
+        method: "POST",
+        url: `/api/posts`,
+        data: {
+            post
+        }
+    })
+)
+export const updatePost = post => (
+    $.ajax({
+        method: "patch",
+        url: `/api/posts/${post.id}`,
+        data: {
+            post
+        }
+    })
+)
+
+export const deletePost = id => (
+    $.ajax({
+        method: "DELETE",
+        url: `/api/posts/${id}`,
+    })
+)
