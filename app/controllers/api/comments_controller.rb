acbe074c75f084d8b0669e92e94c04b3f4c731be
Tam Kar <tkarssli@gmail.com>
2019-03-04 13:13:29 -0800

    Add CommentsController

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,56 @@
 class Api::CommentsController < ApplicationController
+
+    def show 
+        @comment = Comment.find_by(id: params[:id])
+        if @comment
+            render :show, status: 201
+        else
+            render json: "Not Found", status: 404
+        end
+    end
+
+    def index 
+        @comments = Comment.all.where(post_id: params[:post_id])
+        render :index
+    end
+
+    def create 
+        @comment = Comment.new(comment_params)
+        if @comment.save
+            render :show, status: 201
+        else
+            render json: @comment.errors.full_messages, status: 401
+        end
+    end 
+
+    def update 
+        @comment = Comment.find_by(id: params[:id])
+        if @comment.update_attributes(comment_params)
+            @comment.save
+            render :show, status: 201
+        else
+            render json: @comment.errors.full_messages, status: 401
+        end
+
+    end
+
+    def destroy 
+        @comment = Comment.find_by(id: params[:id])
+        @comment.destroy
+        if @comment
+            render :show, status: 201
+        else
+            render json: {
+                message: "No user to log out",
+            },
+            status: 401
+        end
+    end
+
+    private 
+
+    def comment_params
+        params.require(:comment).permit(:body, :user_id, :post_id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
     get 'users/:user_id/posts', :to => 'posts#user_posts'
     resource :session, only: [:create, :destroy]
     # Posts
-    resources :posts, only: [:index, :show, :create, :update, :destroy]
+    resources :posts, only: [:index, :show, :create, :update, :destroy] do 
+      resources :comments, only: [:index]
+    end
+    # Comments
+    resources :comments, only: [:show, :create, :update, :destroy]
 
   end
 
