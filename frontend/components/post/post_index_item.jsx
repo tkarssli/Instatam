55065cca4e3f93c66032ec8234d626d6dd9e7652
Tam Kar <tkarssli@gmail.com>
2019-03-06 12:21:40 -0800

    Add comment count to post index item hover

--- frontend/components/post/post_index_item.jsx
+++ frontend/components/post/post_index_item.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 const PostIndexItem = ({ post, openPostModal }) => (
     <div className = "post-index-item">
-        <div className="post-hover" onClick={() => openPostModal({type: 'post', item: post, scroll: document.documentElement.scrollTop})}></div>
+        <div className="post-hover" onClick={() => openPostModal({type: 'post', item: post, scroll: document.documentElement.scrollTop})}>
+            <div>
+                <span className="comment-count sprite glyph"></span>
+                <span className="bold">{post.commentIds.length}</span>
+            </div>
+        </div>
         <img src={post.photoUrl} alt={post.caption}/>
     </div>
 )
