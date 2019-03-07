1b55cbfd7018afff086584c441137e4735ef27a7
Tam Kar <tkarssli@gmail.com>
2019-03-06 16:04:39 -0800

    Create likes table migration

---
+++ db/migrate/20190307000337_create_likes.rb
@@ -0,0 +1,13 @@
+class CreateLikes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :likes do |t|
+      t.integer :user_id
+      t.integer :post_id
+
+      t.timestamps
+    end
+
+    add_index :likes, :user_id
+    add_index :likes, :post_id
+  end
+end
