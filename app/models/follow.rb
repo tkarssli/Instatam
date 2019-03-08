609d159119b9c50061e5dec05dd9e7d4948616b5
Tam Kar <tkarssli@gmail.com>
2019-03-07 16:31:06 -0800

    Add Validation to follow model

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: follows
+#
+#  id          :bigint(8)        not null, primary key
+#  user_id     :integer          not null
+#  follower_id :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Follow < ApplicationRecord
+    validates :user_id, :follower_id, presence: true
+    validates_uniqueness_of :user_id, :scope => [:follower_id]
+
+    belongs_to :user, foreign_key: :user_id
+    belongs_to :user, foreign_key: :follower_id
+
 end
