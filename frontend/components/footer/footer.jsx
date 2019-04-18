0080c4ec9f0a3ab92156fe031c2ab653c05a5652
Tam Kar <tkarssli@gmail.com>
2019-04-18 12:09:50 -0700

    Add socials to footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -5,18 +5,26 @@ const Footer = () => (
     <footer>
         <ul>
             <li>
-                <Link to="/">ABOUT US</Link>
+                <a target="_blank" href="https://www.tamirkarssli.com">ABOUT US</a>
             </li>
             <li>
-                <Link to="/">SUPPORT</Link>
+                <a target="_blank" href="https://github.com/tkarssli/Instatam/blob/master/README.md">SUPPORT</a>
             </li>
             <li>
-                <Link to="/">API</Link>
+                <a target="_blank" href="https://www.linkedin.com/in/tkarssli/">JOBS</a>
+            </li>
+            <li>
+                <a target="_blank" href="https://github.com/tkarssli/Instatam/wiki/Backend-routes">API</a>
             </li>
             <li>
                 <Link to="/">PRIVACY</Link>
             </li>
-            <li></li>
+            <li className="socials">
+                <a target="_blank" href="https://www.linkedin.com/in/tkarssli/"><div className="github-icon glyph"></div></a>
+                <a target="_blank" href="https://github.com/tkarssli/"><div className="linkedin-icon glyph"></div></a>
+
+            </li>
+
             <li><p>2019 TAMIR KARSSLI</p></li>
         </ul>
     </footer>
